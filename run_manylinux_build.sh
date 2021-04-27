245585e5707c56d849db649bf066472eb455b80e
Hirni-Meshram2 <79629201+Hirni-Meshram2@users.noreply.github.com>
2021-04-27 12:40:06 +0530

    Update run_manylinux_build.sh

--- run_manylinux_build.sh
+++ run_manylinux_build.sh
@@ -40,8 +40,10 @@ echo "arch: $1"
 echo "command: $2"
 #DOCKER_IMAGE=${DOCKER_IMAGE:="quay.io/erotemic/manylinux-for:x86_64-opencv4.1.0-v2"}
 if [ "$1" == "x86_64" ]; then
+    echo "$1"
     DOCKER_IMAGE=${DOCKER_IMAGE:="quay.io/pypa/manylinux2010_x86_64:latest"}
 else
+    echo "$1"
     DOCKER_IMAGE=${DOCKER_IMAGE:="quay.io/pypa/manylinux2014_aarch64:latest"}
 fi    
 # Valid multibuild python versions are:
